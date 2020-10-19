d45dd7b0765c1c6f8ce3ff6ab764e6a264cbf463
LE GOFF <dev.legoff@gmail.com>
2020-10-19 14:24:53 +0200

    chore: chage port

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
+server.listen(8080, () => {
   console.log('JSON Server is running')
 })
